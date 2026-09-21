a19a813abd404d0dbe4f5895445fc95efc5ce09e
WindowHalberdier <isamaraxixa4542@gmail.com>
2026-09-21 11:31:43 +0200

    Import repository files

---
+++ cmd/app/main.go
@@ -0,0 +1,14 @@
+// Build: 72abdc82a738fc7a26f4eac1b1b71b6a
+package main
+
+import "fmt"
+
+func clamp(value, minimum, maximum int) int {
+	if value < minimum { return minimum }
+	if value > maximum { return maximum }
+	return value
+}
+
+func main() {
+	fmt.Println(clamp(12, 0, 10))
+}
---
+++ scripts/build.js
@@ -0,0 +1,5 @@
+// Build: f0a9bac95db94a8f484134590f3a3356
+
+export function clamp(value, minimum, maximum) {
+  return Math.max(minimum, Math.min(maximum, value))
+}
